924f7cb2e2f780759d936a6f6e630f43a7bfa7d2
Aaquib Dilkash <aaquibdilkash@gmail.com>
2021-12-31 20:08:16 +0530

    seo added

--- redux/actions/userActions.js
+++ redux/actions/userActions.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 import {
   CLEAR_ERRORS,
-  PROFILE_GET_FAIL,
-  PROFILE_GET_SUCCESS,
   USER_GET_FAIL,
   USER_GET_SUCCESS,
-  USER_OWN_GET_FAIL,
-  USER_OWN_GET_SUCCESS,
-  USER_SALE_GET_FAIL,
-  USER_SALE_GET_SUCCESS,
-  USER_SAVE_GET_FAIL,
-  USER_SAVE_GET_SUCCESS,
 } from "../constants/UserTypes";
 
 export const userGet = (userId, success = () => {}, failure = () => {}) => async (dispatch) => {
@@ -34,85 +26,6 @@ export const userGet = (userId, success = () => {}, failure = () => {}) => async
   }
 };
 
-export const profileGet = (userId, success = () => {}, failure = () => {}) => async (dispatch) => {
-  try {
-    axios.get(`/api/users/${userId}`).then((res) => {
-      dispatch({
-        type: PROFILE_GET_SUCCESS,
-        payload: res.data.user,
-      });
-
-      success(res.data.user)
-    });
-
-    
-  } catch (e) {
-    dispatch({
-      type: PROFILE_GET_FAIL,
-      payload: e,
-    });
-    failure(e)
-  }
-};
-
-
-export const userSaveGet = (userId, success = () => {}, failure = () => {}) => async (dispatch) => {
-  try {
-    axios.get(`/api/pins?saved=${userId}`).then((res) => {
-      dispatch({
-        type: USER_SAVE_GET_SUCCESS,
-        payload: res.data.pins,
-      });
-      success(res.data.pins)
-    });
-
-    
-  } catch (e) {
-    dispatch({
-      type: USER_SAVE_GET_FAIL,
-      payload: e,
-    });
-    failure(e)
-  }
-};
-export const userOwnGet = (address, success = () => {}, failure = () => {}) => async (dispatch) => {
-  try {
-    axios.get(`/api/pins?owner=${address}`).then((res) => {
-      dispatch({
-        type: USER_OWN_GET_SUCCESS,
-        payload: res.data.pins,
-      });
-      success(res.data.pins)
-    });
-
-    
-  } catch (e) {
-    dispatch({
-      type: USER_OWN_GET_FAIL,
-      payload: e,
-    });
-    failure(e)
-  }
-};
-export const userSaleGet = (address, success = () => {}, failure = () => {}) => async (dispatch) => {
-  try {
-    axios.get(`/api/pins?seller=${address}`).then((res) => {
-      dispatch({
-        type: USER_SALE_GET_SUCCESS,
-        payload: res.data.pins,
-      });
-      success(res.data.pins)
-    });
-
-    
-  } catch (e) {
-    dispatch({
-      type: USER_SALE_GET_FAIL,
-      payload: e,
-    });
-    failure(e)
-  }
-};
 
 export const clearErrors = () => async (dispatch) => {
   dispatch({
